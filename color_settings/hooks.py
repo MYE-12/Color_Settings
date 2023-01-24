792ed1d236fc53e329c8c30e8f532a2c289d1923
mohamedyousuf12 <mohamedyousuffeb12@gmail.com>
2023-01-25 01:37:20 +0530

    Bug Fix

--- color_settings/hooks.py
+++ color_settings/hooks.py
@@ -19,6 +19,7 @@ app_include_js = "/assets/color_settings/js/coloring.js"
 
 # include js, css files in header of web template
 web_include_css = "/assets/color_settings/css/login.css"
+app_include_css = "/assets/color_settings/css/novacept.css"
 # web_include_js = "/assets/color_settings/js/color_settings.js"
 
 # include custom scss in every website theme (without file extension ".scss")
--- color_settings/public/js/coloring.js
+++ color_settings/public/js/coloring.js
@@ -1,17 +1,32 @@
 $(window).on('load', function() {
     frappe.after_ajax(function () {  
-        console.log('ji') 
-        console.log(frappe.boot.color_settings.navbar_background_color)
+        // $(`<div class="novaBrandingClass"><center><p class="novaBrandingText">Powered by</p> </center><a class="novaBrandingAttribute" href="https://novacept.io/" target="_blank"><img class="novaBrandingImage" src="https://i.postimg.cc/xXsCgdFB/color-Logo-Novacept.png" alt="logo" width="80"></a></div>`).insertAfter("#navbar-breadcrumbs")   
+        $(`<div class="novaBrandingClass"><center><p class="novaBrandingText">Powered by</p> </center><a class="novaBrandingAttribute" href="https://novacept.io/" target="_blank"><img class="novaBrandingImage" src="https://i.postimg.cc/xXsCgdFB/color-Logo-Novacept.png" alt="logo" width="80"></a></div>`).insertAfter("#navbar-breadcrumbs")   
+        // $('.novaBrandingClass').css('{position: absolute; left: 0; top:0; right: 0; margin-left: auto; margin-right: auto;display: flex; justify-content: center; font-size:12px;text-align:center;font-weight: bold;}')
         if (frappe.boot.color_settings.navbar_background_color) {
-            $('.navbar').css('background-color',frappe.boot.color_settings.navbar_background_color)
+            $(':root').css('--navbar-bg',frappe.boot.color_settings.navbar_background_color)
+        } 
+        if (frappe.boot.color_settings.primary) {
+            $(':root').css('--nova-primary',frappe.boot.color_settings.primary)
+        } 
+        if (frappe.boot.color_settings.font_color) {
+            $(':root').css('--n-white',frappe.boot.color_settings.font_color)
+        } 
+        if (frappe.boot.color_settings.secondary) {
+            $(':root').css('--nova-secondary',frappe.boot.color_settings.secondary)
         } 
         if (frappe.boot.color_settings.container_bg_color) {
             $(':root').css('--bg-color', frappe.boot.color_settings.container_bg_color);
         }
-        if (frappe.boot.color_settings.container_bg_color) {
+        if (frappe.boot.color_settings.card_color) {
             $(':root').css('--fg-color', frappe.boot.color_settings.card_color);
         }
-        // $(`<div class="novaBrandingClass"><center><p class="novaBrandingText">Powered by</p> </center><a class="novaBrandingAttribute" href="https://novacept.io/" target="_blank"><img class="novaBrandingImage" src="https://i.postimg.cc/xXsCgdFB/color-Logo-Novacept.png" alt="logo" width="80"></a></div>`).insertAfter("#navbar-breadcrumbs")   
+        if (frappe.boot.color_settings.container_bg_color) {
+            $('.layout-main-section').css('--fg-color', frappe.boot.color_settings.card_color);
+        }
+        if (frappe.boot.color_settings.container_bg_color) {
+            var type = 'linear-gradient'
+            $(':root').css('--bg-gradiant',(`linear-gradient(90deg,${frappe.boot.color_settings.container_bg_color},${frappe.boot.color_settings.card_color}`));
+        }
     })
 })
-console.log('hi')
