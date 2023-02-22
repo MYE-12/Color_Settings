2c559ae4cc62aeb2e1b4d7d80df6f59d297c3afa
mohamedyousuf12 <mohamedyousuffeb12@gmail.com>
2023-02-23 01:32:24 +0530

    Auto color change

--- color_settings/color_settings/doctype/color_settings/color_settings.py
+++ color_settings/color_settings/doctype/color_settings/color_settings.py
@@ -9,51 +9,75 @@ from color_settings.api import get_frappe_version
 from frappe.installer import update_site_config
 from frappe.model.document import Document
 
-class ColorSettings(Document):
-	
-	def validate(self):
-		if cint(get_frappe_version()) >= 13:
-			if self.branding_app_name:
-				frappe.db.set_value("Website Settings","Website Settings","app_name",self.branding_app_name)
-			else:
-				if "erpnext" in frappe.get_installed_apps():
-					frappe.db.set_value("System Settings","System Settings","app_name","ERPNext")
-				else:
-					frappe.db.set_value("System Settings","System Settings","app_name","Frappe")
 
-		if frappe.db.exists("DocType","Navbar Settings") and self.application_logo:
-			frappe.db.set_value("Navbar Settings","Navbar Settings","app_logo",self.application_logo)
-			frappe.db.set_value("Website Settings","Website Settings","app_logo",self.favicon_logo)
-			frappe.db.set_value("Website Settings","Website Settings","splash_image",self.favicon_logo)
-			frappe.db.set_value("Website Settings","Website Settings","favicon",self.favicon_logo)
-			# frappe.db.set_value("Navbar Settings","Navbar Settings","logo_width",self.header_logo_width)
-			self.app_logo_set = 1
-			update_site_config("app_logo_url",self.application_logo)
-			frappe.clear_cache()
-		if self.app_logo_set and not self.application_logo:
-			frappe.db.set_value("Navbar Settings","Navbar Settings","app_logo","")
-			frappe.db.set_value("Website Settings","Website Settings","favicon","")
-			frappe.db.set_value("Website Settings","Website Settings","splash_image","")
-			frappe.db.set_value("Website Settings","Website Settings","app_logo","")
-			# frappe.db.set_value("Navbar Settings","Navbar Settings","logo_width",self.header_logo_width)
-			self.app_logo_set = 0
-			update_site_config("app_logo_url",False)
-			frappe.clear_cache()
-   
-		if not self.application_logo:
-			app_logo =frappe.get_hooks("app_logo_url")[1]
-			frappe.errprint(app_logo)
-			frappe.db.set_value("Navbar Settings","Navbar Settings","app_logo",app_logo)
-			update_site_config("app_logo_url",False)
-			frappe.clear_cache()
-   
-		if not self.favicon_logo:
-			app_logo_f =frappe.get_hooks("app_logo_url")[1]
-	  
-			frappe.db.set_value("Website Settings","Website Settings","favicon",app_logo_f)
-			frappe.db.set_value("Website Settings","Website Settings","splash_image",app_logo_f)
-			frappe.db.set_value("Website Settings","Website Settings","app_logo",app_logo_f)
-			frappe.errprint(frappe.get_hooks("app_logo_url")[1])
-			update_site_config("app_logo_url",False)
-   
-			frappe.clear_cache()
+class ColorSettings(Document):
+
+    def validate(self):
+        if cint(get_frappe_version()) >= 13:
+            if self.branding_app_name:
+                frappe.db.set_value(
+                    "Website Settings", "Website Settings", "app_name", self.branding_app_name)
+            else:
+                if "erpnext" in frappe.get_installed_apps():
+                    frappe.db.set_value(
+                        "System Settings", "System Settings", "app_name", "ERPNext")
+                else:
+                    frappe.db.set_value(
+                        "System Settings", "System Settings", "app_name", "Frappe")
+        logo = "/assets/color_settings/images/logo1.png"
+        frappe.errprint(logo)
+
+        if frappe.db.exists("DocType", "Navbar Settings") and self.application_logo:
+            frappe.db.set_value(
+                "Navbar Settings", "Navbar Settings", "app_logo", self.application_logo)
+            frappe.db.set_value(
+                "Website Settings", "Website Settings", "app_logo", self.favicon_logo)
+            frappe.db.set_value(
+                "Website Settings", "Website Settings", "splash_image", self.favicon_logo)
+            frappe.db.set_value(
+                "Website Settings", "Website Settings", "favicon", self.favicon_logo)
+            # frappe.db.set_value("Navbar Settings","Navbar Settings","logo_width",self.header_logo_width)
+            self.app_logo_set = 1
+            update_site_config("app_logo_url", self.application_logo)
+            frappe.clear_cache()
+        if self.app_logo_set and not self.application_logo:
+            frappe.db.set_value("Navbar Settings",
+                                "Navbar Settings", "app_logo", "")
+            frappe.db.set_value("Website Settings",
+                                "Website Settings", "favicon", "")
+            frappe.db.set_value("Website Settings",
+                                "Website Settings", "splash_image", "")
+            frappe.db.set_value("Website Settings",
+                                "Website Settings", "app_logo", "")
+            # frappe.db.set_value("Navbar Settings","Navbar Settings","logo_width",self.header_logo_width)
+            self.app_logo_set = 0
+            update_site_config("app_logo_url", False)
+            frappe.clear_cache()
+
+        if not self.application_logo:
+            app_logo = frappe.get_hooks("app_logo_url")[1]
+            frappe.errprint(app_logo)
+            frappe.db.set_value("Navbar Settings",
+                                "Navbar Settings", "app_logo", app_logo)
+            update_site_config("app_logo_url", False)
+            frappe.clear_cache()
+
+        if not self.favicon_logo:
+            app_logo_f = frappe.get_hooks("app_logo_url")[1]
+            frappe.db.set_value("Website Settings",
+                                "Website Settings", "favicon", app_logo_f)
+            frappe.db.set_value("Website Settings",
+                                "Website Settings", "splash_image", app_logo_f)
+            frappe.db.set_value("Website Settings",
+                                "Website Settings", "app_logo", app_logo_f)
+            frappe.errprint(frappe.get_hooks("app_logo_url")[1])
+            update_site_config("app_logo_url", False)
+
+            frappe.clear_cache()
+
+        if self.navbar_background_color == "#ffffff" or '':
+            frappe.db.set_value("Navbar Settings","Navbar Settings", "app_logo", app_logo)
+            frappe.errprint("LVU")
+        else:
+            frappe.db.set_value("Navbar Settings","Navbar Settings", "app_logo", logo)
+            frappe.errprint("piss")
--- color_settings/public/js/coloring.js
+++ color_settings/public/js/coloring.js
@@ -1,6 +1,14 @@
 $(window).on('load', function () {
     frappe.after_ajax(function () {
+        if (frappe.boot.color_settings.navbar_background_color == "#ffffff" ) {
+        console.log('white')
         $(`<div class="novaBrandingClass"><center><p class="novaBrandingText">Powered by</p> </center><a class="novaBrandingAttribute" href="https://novacept.io/" target="_blank"><img class="novaBrandingImage" src="https://i.postimg.cc/xXsCgdFB/color-Logo-Novacept.png" alt="logo" width="80"></a></div>`).insertAfter("#navbar-breadcrumbs")
+        }
+        else{
+            console.log('black')
+        $(`<div class="novaBrandingClass"><center><p class="novaBrandingText">Powered by</p> </center><a class="novaBrandingAttribute" href="https://novacept.io/" target="_blank"><img class="novaBrandingImage" src="https://i.postimg.cc/w74t2KCC/white-nova-logo.png" alt="logo" width="80"></a></div>`).insertAfter("#navbar-breadcrumbs")
+            
+        }
         if (frappe.boot.color_settings.navbar_background_color) {
             $(':root').css('--navbar-bg', frappe.boot.color_settings.navbar_background_color)
             const hex2rgb = (hex) => { 
@@ -8,11 +16,13 @@ $(window).on('load', function () {
                 return fullHex(hex); 
             } 
             const r = parseInt(hex.slice(1, 3), 16); 
+            console.log(r)
             const g = parseInt(hex.slice(3, 5), 16); 
             const b = parseInt(hex.slice(5, 7), 16); 
             return { r, g, b }; 
             }
             const rgb = hex2rgb(frappe.boot.color_settings.navbar_background_color)
+            console.log(rgb)
             $(':root').css('--red',rgb.r)
             $(':root').css('--green',rgb.g)
             $(':root').css('--blue',rgb.b)
@@ -21,11 +31,21 @@ $(window).on('load', function () {
         if (frappe.boot.color_settings.primary) {
             $(':root').css('--nova-primary', frappe.boot.color_settings.primary)
         }
-        if (frappe.boot.color_settings.font_color) {
-            $(':root').css('--n-white', frappe.boot.color_settings.font_color)
-        }
         if (frappe.boot.color_settings.secondary) {
             $(':root').css('--nova-secondary', frappe.boot.color_settings.secondary)
+            const hex2rgb1 = (hex1) => { 
+                if (hex1.length === 4) { 
+                return fullHex1(hex1); 
+            } 
+            const r = parseInt(hex1.slice(1, 3), 16); 
+            const g = parseInt(hex1.slice(3, 5), 16); 
+            const b = parseInt(hex1.slice(5, 7), 16); 
+            return { r, g, b }; 
+            }
+            const rgb = hex2rgb1(frappe.boot.color_settings.secondary)
+            $(':root').css('--red1',rgb.r)
+            $(':root').css('--green1',rgb.g)
+            $(':root').css('--blue1',rgb.b)
         }
         if (frappe.boot.color_settings.header_logo_width) {
             $('.app-logo').css('width', frappe.boot.color_settings.header_logo_width + 'px');
